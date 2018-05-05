de7ca51095863c809cf046375bf98b6828008044
maolin <772984968@qq.com>
2018-05-05 14:50:16 +0800

    Basic user profile editing

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
             $table->timestamps();
         });
     }
